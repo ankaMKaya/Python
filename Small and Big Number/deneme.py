b902bdffbae0a80d06e472e775fbba92fdb79eef
MKaya <102487435+ankaMKaya@users.noreply.github.com>
2023-07-01 00:06:22 +0300

    Add files via upload

---
+++ Small and Big Number/deneme.py
@@ -0,0 +1,11 @@
+
+sayi = int(input("Lütfen bir sayi giriniz: "))
+
+if sayi < 10:
+    print("sayimiz 10 dan küçüktür")
+
+if sayi > 10:
+    print("sayimiz 10 dan büyüktür")
+
+if sayi == 10:
+    print("sayimiz 10 dur ve eşittir")
